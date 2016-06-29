41c6fe9fa8fb9e3310e2a68a77daca481bca2ab9
Vincent Lo <vincentsylo@gmail.com>
2016-06-29 22:37:35 +1000

    Initial commit

---
+++ web/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './containers/App';
+import userTypes from '../../core/src/constants/userTypes';
+
+ReactDOM.render(<App />, document.getElementById('root'));
