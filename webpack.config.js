e34b9468ed3b1ef4bd72670251e11e427e3d61c8
Vincent Lo <vincentsylo@gmail.com>
2016-07-02 22:08:54 +1000

    Enable devDependencies on Heroku

--- webpack.config.js
+++ webpack.config.js
@@ -1,2 +1,2 @@
-require('dotenv').config();
+require('dotenv').config({ silent: true }); // Heroku builds will not have a .env
 module.exports = process.env.NODE_ENV === 'production' ? require('./webpack.production.config.js') : require('./webpack.development.config.js');
